fe3f95a244a25cb668dc9fa13798e854e0eed072
Mocha1530 <astherielle360@gmail.com>
2026-07-26 06:22:49 +0800

    feat: added uptime record from betterstack

--- src/commands/utility/status.ts
+++ src/commands/utility/status.ts
@@ -9,6 +9,7 @@ import { discord_api } from "@/utils/discord-api";
 import { MessageFlags } from "discord.js";
 import os from "os";
 import { formattedISODate } from "@/utils/formatDate";
+import axios from "axios";
 
 export const register = new SlashCommandBuilder()
   .setName("status")
@@ -16,9 +17,14 @@ export const register = new SlashCommandBuilder()
 
 export const execute: executeCommand = async (interaction) => {
   // This part gets API latency
-  const start = Date.now();
-  await discord_api.get("/users/@me");
-  const apiLatency = Date.now() - start;
+  let apiLatency = 0;
+  try {
+    const start = Date.now();
+    await discord_api.get("/users/@me");
+    apiLatency = Date.now() - start;
+  } catch {
+    apiLatency = -1;
+  }
 
   // This the collects serverless metrics
   const memory = process.memoryUsage();
@@ -28,6 +34,16 @@ export const execute: executeCommand = async (interaction) => {
   const hostRamGB = (os.totalmem() / 1024 / 1024 / 1024).toFixed(2);
   const uptimeSeconds = Math.floor(process.uptime());
   const vercelRegion = process.env.VERCEL_REGION || "local";
+
+  const sla = await fetchUptime();
+  let uptimeSla = `\n**Uptime (Last 30 days)**`;
+  if (sla) {
+    uptimeSla =
+      uptimeSla +
+      `\n- **Availability:** \`${sla.availability.toFixed(2)}%\`\n- **Downtime:** \`${sla.total_downtime} seconds\`\n- **Incidents:** \`${sla.number_of_incidents}\`\n- **Longest Incident:** \`${sla.longest_incident} seconds\``;
+  } else {
+    uptimeSla = uptimeSla + "\n- Unavailable";
+  }
 
   const container = new ContainerBuilder()
     .setAccentColor(9166826)
@@ -39,7 +55,7 @@ export const execute: executeCommand = async (interaction) => {
     )
     .addTextDisplayComponents(
       new TextDisplayBuilder().setContent(
-        `>>> **Discord API Latency:** \`${apiLatency}ms\`\n**Instance Uptime:** \`${uptimeSeconds}s\`\n**Memory Usage (Process RSS):** \`${rssMB} MB\` used out of \`1024 MB\` limit\n**Heap (Used / Total):** \`${heapUsedMB} MB / ${heapTotalMB} MB\`\n**Host RAM:** \`${hostRamGB} GB\`\n**Server Region:** \`${vercelRegion}\``,
+        `>>> **Discord API Latency:** \`${apiLatency}ms\`\n**Instance Uptime:** \`${uptimeSeconds}s\`\n**Memory Usage (Process RSS):** \`${rssMB} MB\` used out of \`1024 MB\` limit\n**Heap (Used / Total):** \`${heapUsedMB} MB / ${heapTotalMB} MB\`\n**Host RAM:** \`${hostRamGB} GB\`\n**Server Region:** \`${vercelRegion}\`${uptimeSla}`,
       ),
     )
     .addSeparatorComponents(new SeparatorBuilder().setDivider(false))
@@ -57,3 +73,47 @@ export const execute: executeCommand = async (interaction) => {
     },
   };
 };
+
+async function fetchUptime() {
+  const monitorId = "4724536";
+  const apiKey = process.env.UPDATE_READ_KEY!;
+
+  if (!monitorId || !apiKey) {
+    return null;
+  }
+
+  const to = new Date();
+  const from = new Date(to);
+  from.setDate(from.getDate() - 30);
+
+  const formatDate = (date: Date) => date.toISOString().split("T")[0];
+
+  try {
+    const uptimeRes = await axios.get<UptimeSLA>(
+      `https://uptime.betterstack.com/api/v2/monitors/${monitorId}/sla`,
+      {
+        params: { from: formatDate(from), to: formatDate(to) },
+        headers: { Authorization: `Bearer ${apiKey}` },
+        timeout: 5000,
+      },
+    );
+
+    return uptimeRes.data.data.attributes;
+  } catch {
+    return null;
+  }
+}
+
+interface UptimeSLA {
+  data: {
+    id: string;
+    type: string;
+    attributes: {
+      availability: number;
+      total_downtime: number;
+      number_of_incidents: number;
+      longest_incident: number;
+      average_incident: number;
+    };
+  };
+}
